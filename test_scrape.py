ffa83941bbeeeefbd447984f77615fc802edfd0d
jnadach <jan.nadachowski@gmail.com>
2023-02-18 23:22:06 +0100

    adding other parlement trems

--- test_scrape.py
+++ test_scrape.py
@@ -3,6 +3,16 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
+# https://www.sejm.gov.pl/Sejm9.nsf/poslowie.xsp?type=A
+# https://www.sejm.gov.pl/Sejm9.nsf/posel.xsp?id=001&type=A
+#
+#
+# https://www.sejm.gov.pl/Sejm8.nsf/poslowie.xsp?type=P
+# https://www.sejm.gov.pl/Sejm8.nsf/posel.xsp?id=001&type=P
+#
+# https://www.sejm.gov.pl/Sejm7.nsf/poslowie.xsp?type=P
+# https://www.sejm.gov.pl/Sejm7.nsf/posel.xsp?id=001&type=P
+
 
 def get_pm_name(soup):
     return soup.find('h1').text
@@ -10,7 +20,7 @@ def get_pm_name(soup):
 
 def get_value(soup, text):
     try:
-        tag = soup.find_all(text=text)
+        tag = soup.find_all(string=text)
         value = tag[0].parent.find_next_sibling().text
         return value
     except:
@@ -27,11 +37,11 @@ def get_pm_profile(soup, variable_list):
 
     return pm_profile
 
-def get_all_pm_profiles(variable_list):
+def get_all_pm_profiles(parliament_term, type, variable_list):
     all_pm_profiles = []
     for i in range(1, 461):
         no_with_zeros = str(i).zfill(3)
-        url = f'https://www.sejm.gov.pl/Sejm9.nsf/posel.xsp?id={no_with_zeros}&type=A'
+        url = f'https://www.sejm.gov.pl/Sejm{parliament_term}.nsf/posel.xsp?id={no_with_zeros}&type={type}'
         soup = BeautifulSoup(requests.get(url).text, 'lxml')
         pm_profile = get_pm_profile(soup, variable_list)
         all_pm_profiles.append(pm_profile)
@@ -61,7 +71,20 @@ variable_list = ["Wybran.* dnia:", "Lista:", "Okręg wyborczy:", "Liczba głosó
                 "Ślubowanie:", "Staż parlamentarny:", "Klub/koło:", "Data i miejsce urodzenia:",
                 "Wykształcenie:", "Ukończona szkoła:", "Zawód:"]
 
-pm_profiles = get_all_pm_profiles(variable_list)
-pm_profiles_df = pm_profiles_to_pandas(pm_profiles)
+print("Dane 9 kadencja semju:")
+pm_profiles_9 = get_all_pm_profiles(9, "A", variable_list)
+pm_profiles_9_df = pm_profiles_to_pandas(pm_profiles_9)
 
-print(pm_profiles_df)
+print(pm_profiles_9_df)
+
+print("Dane 8 kadencja semju:")
+pm_profiles_8 = get_all_pm_profiles(8, "P", variable_list)
+pm_profiles_8_df = pm_profiles_to_pandas(pm_profiles_8)
+
+print(pm_profiles_8_df)
+
+print("Dane 7 kadencja semju:")
+pm_profiles_7 = get_all_pm_profiles(7, "P", variable_list)
+pm_profiles_7_df = pm_profiles_to_pandas(pm_profiles_9)
+
+print(pm_profiles_9_df)
